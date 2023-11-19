5a98238f91b83e188a9b9d074e489b7abb36fd14
Louis Joseph <louisjosephleadway@gmail.com>
2023-11-19 09:22:29 +0100

    blackjack

--- building_blackjack/index.js
+++ building_blackjack/index.js
@@ -1,5 +1,6 @@
 
 document.getElementById('startGame').onclick = startGame
+document.getElementById('newCard').onclick = newCard
 
 let firstCard = 3
 let secondCard = 8
@@ -9,10 +10,18 @@ let hasBlacJack = false
 let isAlive = true
 let message = ''
 
+
+// 1. store the message-el pararaph in a variable called messageEl
 let messageEl = document.getElementById('message-el')
 let sumEl = document.getElementById('sum-el')
+let cardsEl = document.getElementById('cards-el')
 
-function startGame() {
+function startGame (){
+  renderGame()
+}
+
+function renderGame() {
+  cardsEl.textContent = "Cards: " + firstCard + ", " + secondCard
   sumEl.textContent = 'Sum: ' +sum
   
   if (sum <= 20) {
@@ -26,10 +35,21 @@ function startGame() {
    message = "you're out of the game!"
    isAlive = false
   }
+
+  //2. return the messageEL using message.El.textContent
   messageEl.textContent = message
 
 
 }
+
+function newCard(){
+  console.log("Drawing a new card from deck.")
+  let card = 5
+  sum += card
+  renderGame()
+}
+
+
 
 
 
